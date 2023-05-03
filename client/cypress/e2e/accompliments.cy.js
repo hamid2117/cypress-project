9676d1cd0661f15ef5af18afcd2694a2894c273d
hamid2117 <saim008865@gmail.com>
2023-05-03 09:27:55 +0500

    test: done accomplishment

--- client/cypress/e2e/accompliments.cy.js
+++ client/cypress/e2e/accompliments.cy.js
@@ -18,24 +18,20 @@ describe('Accomplishment dashboard', () => {
     cy.get("[placeholder='My accomplishment...']").type('I use Vim! 💪')
     cy.get("[type='checkbox']").click()
     cy.get('button').click()
-    cy.contains('This Accomplishment was Sucessfully Submitted').should(
+    cy.contains('This Accomplisment was Successfully Submitted').should(
       'be.visible'
     )
   })
 
-  //  it('should display inappropriate content error when text that includes giraffe is submitted (with mock)', () => {
-  //    cy.intercept('POST', 'http://localhost:4000', (req) => {
-  //      req.reply((res) => {
-  //        res.send({
-  //          msg: 'Your content is not appropriate',
-  //        })
-  //      })
-  //    })
-
-  //    cy.get("[placeholder='Title']").type('This is my accomplishment')
-  //    cy.get("[placeholder='My accomplishment...']").type('I use Vim! 💪')
-  //    cy.get("[type='checkbox']").click()
-  //    cy.get('button').click()
-  //    cy.contains('Your content is not appropriate').should('be.visible')
-  //  })
+  it('After submission should go back to Accomplishment page ', () => {
+    cy.get("[placeholder='Title']").type('This is my accomplishment')
+    cy.get("[placeholder='My accomplishment...']").type('I use Vim! 💪')
+    cy.get("[type='checkbox']").click()
+    cy.get('button').click()
+    cy.contains('This Accomplisment was Successfully Submitted').should(
+      'be.visible'
+    )
+    cy.contains('Back').click()
+    cy.get("[placeholder='Title']").should('be.visible')
+  })
 })
